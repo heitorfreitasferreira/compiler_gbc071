1a41da363ecc9260bc71fb6f36dfab0c4ced4361
heitorfreitasferreira <heitor.ff@hotmail.com>
2024-08-25 15:11:23 -0300

    feat: refactor lexer to use symbol table and state machine

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -3,26 +3,46 @@ package lexer
 import (
 	"bufio"
 
+	simboltable "github.com/heitorfreitasferreira/compiler/simbol_table"
+	statemachine "github.com/heitorfreitasferreira/compiler/state_machine"
 	"github.com/heitorfreitasferreira/compiler/types"
 )
 
 type Lexer struct {
-	types.Position
+	charsRead    int
+	lastByteRead byte
+	statemachine.AFND
+	*types.Position
+	lastPosition *types.Position
 	*bufio.Reader
+
+	*simboltable.SymbolTable
 }
 
 func NewLexer(reader *bufio.Reader) *Lexer {
 	return &Lexer{
-		Reader: reader,
-		Position: types.Position{
+		Reader:    reader,
+		charsRead: 0,
+		Position: &types.Position{
 			Line:   1,
 			Column: 0,
 		},
 	}
 }
 
-func (l *Lexer) GetNextToken() (types.Token, error) {
-	return types.Token{}, nil
+func (l *Lexer) GetNextToken() (*types.Token, error) {
+	return nil, nil // TODO: Implement
+}
+
+func (l *Lexer) DealWithLookAhead() error {
+	l.charsRead--
+	l.Position.Column = l.lastPosition.Column
+	l.Position.Line = l.lastPosition.Line
+	err := l.Reader.UnreadRune()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (l *Lexer) peak() (rune, error) {
