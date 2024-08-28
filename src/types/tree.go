59d45cbd31db60112a91a647be31b45f10e01ccd
heitorfreitasferreira <heitor.ff@hotmail.com>
2024-08-28 20:26:54 -0300

    refactor: Update Tree struct to use generic type parameter
    This commit updates the Tree struct in tree.go to use a generic type parameter instead of the specific type "int". This change allows for more flexibility in using different types of values in the tree.

--- src/types/tree.go
+++ src/types/tree.go
@@ -1,53 +1,29 @@
 package types
 
-type node[T any] struct {
-	Value    T
-	Children []*node[T]
+type node[T comparable] struct {
+	value    T
+	children []*node[T]
 }
 
-type BinTree struct {
-	root *node[int]
+type Tree[T comparable] struct {
+	root *node[T]
 }
 
-func (bt *BinTree) Add(value int) bool {
-	if bt.root == nil {
-		bt.root = &node[int]{Value: value}
-		return true
-	}
-	return bt.add(value, bt.root)
+func (t Tree[T]) Find(value T) (T, bool) {
+	return t.find(t.root, value)
 }
-func (bt *BinTree) add(value int, childNode *node[int]) bool {
-	if value == childNode.Value {
-		return false
+
+func (t Tree[T]) find(n *node[T], value T) (T, bool) {
+	if n == nil {
+		return value, false
 	}
-	if value < childNode.Value {
-		if childNode.Children[0] == nil {
-			childNode.Children[0] = &node[int]{Value: value}
-			return true
+	if n.value == value {
+		return value, true
+	}
+	for _, child := range n.children {
+		if v, ok := t.find(child, value); ok {
+			return v, true
 		}
-		return bt.add(value, childNode.Children[0])
-	} else {
-		if childNode.Children[1] == nil {
-			childNode.Children[1] = &node[int]{Value: value}
-			return true
-		}
-		return bt.add(value, childNode.Children[1])
 	}
-}
-
-func (bt *BinTree) Get(value int) bool {
-	return bt.get(value, bt.root)
-}
-
-func (bt *BinTree) get(value int, childNode *node[int]) bool {
-	if childNode == nil {
-		return false
-	}
-	if value == childNode.Value {
-		return true
-	}
-	if value < childNode.Value {
-		return bt.get(value, childNode.Children[0])
-	}
-	return bt.get(value, childNode.Children[1])
+	return value, false
 }
