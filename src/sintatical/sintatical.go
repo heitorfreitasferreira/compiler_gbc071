5624088dd336d25353af09b1b3cca8b2936cc805
heitorfreitasferreira <heitor.ff@hotmail.com>
2024-10-01 19:52:24 -0300

    fix: considerando fim de arquivo

--- src/sintatical/sintatical.go
+++ src/sintatical/sintatical.go
@@ -1,6 +1,8 @@
 package sintatical
 
 import (
+	"fmt"
+
 	"github.com/heitorfreitasferreira/compiler/lexer"
 	"github.com/heitorfreitasferreira/compiler/types"
 )
@@ -19,6 +21,12 @@ func (sin *Sintatical) Analize() (ConcreteSintaticalTree, error) {
 	proxToken = sin.lex.GetNextToken()
 
 	err := s(tree.Root, sin.lex)
-
-	return tree, err
+	if err != nil {
+		return tree, err
+	}
+	proxToken = sin.lex.GetNextToken()
+	if proxToken.TokenType != types.EOF {
+		return tree, fmt.Errorf("expected EOF at %v", proxToken.Position)
+	}
+	return tree, nil
 }
