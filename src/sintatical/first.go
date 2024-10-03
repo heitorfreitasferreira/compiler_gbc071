f74b2c1aa1b1c8a8e6cbecf3832e5874fde34cf5
heitorfreitasferreira <heitor.ff@hotmail.com>
2024-10-03 10:14:01 -0300

    Refactor first.go to update FIRST sets

--- src/sintatical/first.go
+++ src/sintatical/first.go
@@ -6,19 +6,21 @@ var first = map[types.GrammarSymbol][]types.TokenType{
 	S:             {types.KW_MAIN},
 	BLOCK:         {types.KW_BEGIN},
 	LIST:          {types.IDENTIFIER},
-	LIST_PRIME:    {types.KKOMA},                                   // WHILE
-	DECL:          {types.KW_TYPE},                                 // WHILE
-	CMD_SEQ:       {types.KW_IF, types.KW_WHILE, types.IDENTIFIER}, // WHILE
+	LIST_PRIME:    {types.KKOMA},   // WHILE
+	DECL:          {types.KW_TYPE}, // WHILE
+	CMD:           {types.KW_IF, types.KW_WHILE, types.KW_REPEAT, types.IDENTIFIER},
+	CMD_SEQ:       {types.KW_IF, types.KW_WHILE, types.IDENTIFIER, types.KW_REPEAT}, // WHILE
 	CMD_OR_BLOCK:  {types.KW_IF, types.KW_WHILE, types.KW_REPEAT, types.IDENTIFIER, types.KW_BEGIN},
 	CMD_SEL:       {types.KW_IF},
 	CMD_SEL_PRIME: {types.KW_ELSE},
 	CMD_REP:       {types.KW_WHILE, types.KW_REPEAT},
+	CMD_ATR:       {types.IDENTIFIER},
 	EXP:           {types.START_PAREN, types.CONST},
 	EXP_PRIME:     {types.ARIOP_SUM}, //WHILE
 	TERM:          {types.START_PAREN, types.CONST},
 	TERM_PRIME:    {types.ARIOP_MULT}, //WHILE
 	FACTOR:        {types.START_PAREN, types.CONST},
-	FACTOR_PRIME:  {types.RELOP}, //WHILE
+	FACTOR_PRIME:  {types.ARIOP_POW}, //WHILE
 	COND:          {types.START_PAREN, types.CONST},
 }
 
