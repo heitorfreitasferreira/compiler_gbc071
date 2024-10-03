b70052141998e63577ad6302d209f1e355b3a981
heitorfreitasferreira <heitor.ff@hotmail.com>
2024-10-03 00:01:04 -0300

    feat: stringify method in tree.go

--- src/sintatical/tree.go
+++ src/sintatical/tree.go
@@ -1,17 +1,29 @@
 package sintatical
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/heitorfreitasferreira/compiler/types"
 )
 
 type ConcreteSintaticalTree types.Tree[types.GrammarSymbol]
 
-func appendDown(currNode *types.Node[types.GrammarSymbol], tk NonTerminal) {
-
-	oldNode := currNode
-	currNode = &types.Node[types.GrammarSymbol]{
-		Value:    tk,
-		Children: []*types.Node[types.GrammarSymbol]{},
+func (t ConcreteSintaticalTree) stringify(n *types.Node[types.GrammarSymbol], depth int) string {
+	if n == nil {
+		return ""
 	}
-	oldNode.Children = append(oldNode.Children, currNode)
+	result := fmt.Sprintf("%s%v\n", strings.Repeat("  ", depth), n.Value)
+	for _, child := range n.Children {
+		result += t.stringify(child, depth+1)
+	}
+	return result
+}
+
+func (t ConcreteSintaticalTree) String() string {
+	return t.stringify(t.Root, 0)
+}
+
+func (cst ConcreteSintaticalTree) IsEqual(other *ConcreteSintaticalTree) bool {
+	return cst.Root.IsEqual(other.Root)
 }
